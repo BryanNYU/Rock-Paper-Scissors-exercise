4a7e3a581f0b569c50aa17d1b7a68ce288979602
Bryan Tran Lu <btl245@stern.nyu.edu>
2019-06-05 18:56:21 -0400

    Capture inputs

--- Game.py
+++ Game.py
@@ -1,3 +1,16 @@
 # game.py
 
-print("Rock, Paper, Scissors, Shoot!")
+print("Rock, Paper, Scissors, Shoot!") # this is also a comment
+
+# CAPTURE INPUT
+user_choice = input("Please choice one of the following options: 'rock', 'paper', or 'scissors' (without the quotes):")
+print("------------------------")
+print("YOU CHOSE", user_choice)
+
+# VALIDATE INPUTS
+
+# GENERATE COMPUTER SELECTION
+
+# DETERMINE THE WINNER
+
+# DISPLAY FINAL OUTPUTS / OUTCOMES
